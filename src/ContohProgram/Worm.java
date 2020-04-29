408ba12e0d7b5047d7f7f7fefdd621109ce1e5b2
rian <riandiuniska@gmail.com>
2020-04-29 15:30:14 +0800

    Attribute & Behavior

---
+++ src/ContohProgram/Worm.java
@@ -0,0 +1,17 @@
+package ContohProgram;
+
+public class Worm {
+    //attribute di dalam class, yang mana cetakannya ada didalam kelas dan nilai nya di dalam objek
+    String userName;
+    int score;
+    double length;
+
+    //behavior/method/objek/yang bisa dikerjakan oleh objek(cacing)
+    void eatSnack(){
+        System.out.println("Nom... Nom... Nom... Nom......");
+    }
+
+    void eatOthers(Worm worm){
+        score += 500;
+    }
+}
