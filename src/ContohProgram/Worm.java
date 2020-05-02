43f65d1b57ca65e2c02097b853f20cd09c3d887d
rian <riandiuniska@gmail.com>
2020-05-02 12:36:39 +0800

    Attribute & Behavior (Worm Zone)

--- src/ContohProgram/Worm.java
+++ src/ContohProgram/Worm.java
@@ -1,17 +1,17 @@
 package ContohProgram;
 
 public class Worm {
-    //attribute di dalam class, yang mana cetakannya ada didalam class dan nilai nya di dalam objek
+    //attribute di dalam class, yang mana cetakannya ada didalam class dan nilai nya di dalam objek.
     String userName;
     int score;
     double length;
 
-    //behavior/method/objek/yang bisa dikerjakan oleh objek(cacing)
+    //behavior/method/objek/yang bisa dikerjakan oleh objek(cacing).
     void eatSnack(){
         System.out.println("Nom... Nom... Nom... Nom......");
     }
 
-    void eatOthers(Worm worm) { //memakan sesama cacing
+    void eatOthers(Worm worm) { //memakan sesama cacing.
         score += 500;
     }
 }
